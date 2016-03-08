0943acd03dbff7a7c87c82ae259a0a81834e4dcb
versemonger <halinzhuo@gmail.com>
2016-03-07 22:07:01 -0700

    Add renamed script

---
+++ extract_feature_and_train.py
@@ -0,0 +1,79 @@
+"""
+Adapted from https://www.kaggle.com/wenxuanchen/home-depot-product-search-relevance/sklearn-random-forest
+This script basically counts the occurrences of each word of search
+term in different part of information of the products.
+"""
+
+
+import pandas as pd
+from sklearn.ensemble \
+    import RandomForestRegressor, BaggingRegressor
+
+
+def find_occurrences(str1, str2):
+    """
+    find in str2 occurrences of each word in str1
+    example: str1 = "good job", str2 = "good good job"
+    it gets 2(for good) and 1(for job), they their sum
+    is 3
+    :param str1:
+    :param str2:
+    :return:
+    """
+    return sum(str2.count(word) for word in str1.split())
+
+# import the number of training tuples
+df_train = pd.read_csv("train.csv", encoding="ISO-8859-1")
+train_num = df_train.shape[0]
+df_train = None
+
+df_all = pd.read_pickle('df_all')
+
+# Coalesce all information into one column so we can apply
+# map to that one column
+df_all['product_info'] \
+    = df_all['search_term'] + "\t" + df_all['product_title']\
+    + "\t" + df_all['product_description'] + "\t"\
+    + df_all['attributes'] + "\t" + df_all['brand']
+
+# map find_occurrences to the separated information.
+df_all['word_in_title'] = df_all['product_info']\
+    .map(lambda x: find_occurrences(x.split('\t')[0],
+                                    x.split('\t')[1]))
+df_all['word_in_description'] = df_all['product_info']\
+    .map(lambda x: find_occurrences(x.split('\t')[0],
+                                    x.split('\t')[2]))
+df_all['word_in_attributes'] = df_all['product_info']\
+    .map(lambda x: find_occurrences(x.split('\t')[0],
+                                    x.split('\t')[3]))
+df_all['word_in_brand'] = df_all['product_info']\
+    .map(lambda x: find_occurrences(x.split('\t')[0],
+                                    x.split('\t')[4]))
+df_all = df_all.drop(['search_term', 'product_title',
+                      'product_description', 'product_info',
+                      'attributes', 'brand'], axis=1)
+
+# extract train data and test data
+df_train = df_all.iloc[:train_num]
+df_test = df_all.iloc[train_num:]
+id_test = df_test['id']
+
+y_train = df_train['relevance'].values
+X_train = df_train.drop(['id', 'relevance'], axis=1).values
+X_test = df_test.drop(['id', 'relevance'], axis=1).values
+
+
+# train the data
+rf = RandomForestRegressor(
+        n_estimators=15, max_depth=6, random_state=0)
+clf = BaggingRegressor(
+        rf, n_estimators=45, max_samples=0.1, random_state=25)
+clf.fit(X_train, y_train)
+y_prediction = clf.predict(X_test)
+
+
+# Output the result
+pd.DataFrame({"id": id_test, "relevance": y_prediction})\
+    .to_csv('submission.csv', index=False)
+
+
