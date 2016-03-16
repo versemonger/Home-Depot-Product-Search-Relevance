1d6761a943cc0af4af1130b57b05eadbc60de029
versemonger <halinzhuo@gmail.com>
2016-03-16 14:14:30 -0600

    Normalize the data, save part of data to a validation data set

--- extract_feature_and_train.py
+++ extract_feature_and_train.py
@@ -4,12 +4,18 @@ This script basically counts the occurrences of each word of search
 term in different part of information of the products.
 """
 
-
 import pandas as pd
 from sklearn.ensemble \
     import RandomForestRegressor, BaggingRegressor
 from sklearn.datasets import dump_svmlight_file
 import numpy as np
+from nltk.corpus import stopwords
+import store_data_in_pandas as sd
+import sys
+
+stopwords_list = stopwords.words('english')
+stemmed_stopwords = [sd.stem_text(stop_word)
+                     for stop_word in stopwords_list]
 
 
 def find_occurrences(str1, str2):
@@ -22,66 +28,113 @@ def find_occurrences(str1, str2):
     :param str2:
     :return:
     """
-    return sum(str2.count(word) for word in str1.split())
-
-# import the number of training tuples
-df_train = pd.read_csv("train.csv", encoding="ISO-8859-1")
-train_num = df_train.shape[0]
-df_train = None
-
-df_all = pd.read_pickle('df_all')
-
-# Coalesce all information into one column so we can apply
-# map to that one column
-df_all['product_info'] \
-    = df_all['search_term'] + "\t" + df_all['product_title']\
-    + "\t" + df_all['product_description'] + "\t"\
-    + df_all['attributes'] + "\t" + df_all['brand']
-
-# map find_occurrences to the separated information.
-df_all['word_in_title'] = df_all['product_info']\
-    .map(lambda x: find_occurrences(x.split('\t')[0],
-                                    x.split('\t')[1]))
-df_all['word_in_description'] = df_all['product_info']\
-    .map(lambda x: find_occurrences(x.split('\t')[0],
-                                    x.split('\t')[2]))
-df_all['word_in_attributes'] = df_all['product_info']\
-    .map(lambda x: find_occurrences(x.split('\t')[0],
-                                    x.split('\t')[3]))
-df_all['word_in_brand'] = df_all['product_info']\
-    .map(lambda x: find_occurrences(x.split('\t')[0],
-                                    x.split('\t')[4]))
-df_all = df_all.drop(['search_term', 'product_title',
-                      'product_description', 'product_info',
-                      'attributes', 'brand'], axis=1)
-
-# extract train data and test data
-df_train = df_all.iloc[:train_num]
-df_test = df_all.iloc[train_num:]
-id_test = df_test['id']
-
-y_train = df_train['relevance'].values
-X_train = df_train.drop(
-        ['id', 'relevance', 'product_uid'], axis=1).values
-X_test = df_test.drop(
-        ['id', 'relevance', 'product_uid'], axis=1).values
-X_test_len = len(X_test)
-
-rf = RandomForestRegressor(
-        n_estimators=30, max_depth=6, random_state=0)
-clf = BaggingRegressor(
-        rf, n_estimators=60, max_samples=0.13, random_state=25)
-clf.fit(X_train, y_train)
-y_prediction = clf.predict(X_test)
-
-# Output the result
-pd.DataFrame({"id": id_test, "relevance": y_prediction})\
-    .to_csv('submission.csv', index=False)
+    return sum([str2.count(word) for word in str1.split()
+                if word not in stemmed_stopwords])
 
 
-# output the result to a libSVM file.
-dump_svmlight_file(X_train, y_train, 'train_libSVM.dat',
-                   zero_based=True, multilabel=False)
-test_file_label = np.zeros(X_test_len)
-dump_svmlight_file(X_test, test_file_label, 'test_libSVM.dat',
-                   zero_based=True, multilabel=False)
+def range_filter(x):
+    """
+    For each feature, round to to large data to 6
+    :param x: feature in one sample
+    :return: x if x is small and 6 if x is larger than 6
+    """
+    if x > 6:
+        return 6
+    else:
+        return x
+
+
+def main():
+    # If the script has at least one additional argument,
+    # we train the model and generate output file.
+    # Otherwise we just output data to libSVM file
+    train_model = False
+    if len(sys.argv) >= 2:
+        train_model = True
+
+    # import the number of training tuples
+    df_train = pd.read_csv("train.csv", encoding="ISO-8859-1")
+    train_num = df_train.shape[0]
+    df_train = None
+
+    df_all = pd.read_pickle('df_all')
+
+    # Coalesce all information into one column so we can apply
+    # map to that one column
+    df_all['product_info'] \
+        = df_all['search_term'] + "\t" + df_all['product_title'] \
+          + "\t" + df_all['product_description'] + "\t" \
+          + df_all['attributes'] + "\t" + df_all['brand']
+
+    # map find_occurrences to the separated information.
+    df_all['word_in_title'] = df_all['product_info'] \
+        .map(lambda x: find_occurrences(x.split('\t')[0],
+                                        x.split('\t')[1]))
+    df_all['word_in_description'] = df_all['product_info'] \
+        .map(lambda x: find_occurrences(x.split('\t')[0],
+                                        x.split('\t')[2]))
+    df_all['word_in_attributes'] = df_all['product_info'] \
+        .map(lambda x: find_occurrences(x.split('\t')[0],
+                                        x.split('\t')[3]))
+    df_all['word_in_brand'] = df_all['product_info'] \
+        .map(lambda x: find_occurrences(x.split('\t')[0],
+                                        x.split('\t')[4]))
+    df_all = df_all.drop(['search_term', 'product_title',
+                          'product_description', 'product_info',
+                          'attributes', 'brand'], axis=1)
+
+    # Normalize all useful data in df
+    for column in ['word_in_title', 'word_in_description',
+                   'word_in_attributes', 'word_in_brand']:
+        df_all[column] \
+            = df_all[column].map(lambda x: range_filter(x))
+        mean_word_in_title = df_all[column].mean()
+        std_word_in_title = df_all[column].std()
+        df_all[column] \
+            = df_all[column] \
+            .map(
+            lambda x: (x - mean_word_in_title) / std_word_in_title)
+
+    # extract train data and test data
+    df_train = df_all.iloc[:train_num]
+    df_test = df_all.iloc[train_num:]
+    id_test = df_test['id']
+    id_test.to_pickle('id_test')
+
+    y_train = df_train['relevance'].values
+    X_train = df_train.drop(
+            ['id', 'relevance', 'product_uid'], axis=1).values
+    X_test = df_test.drop(
+            ['id', 'relevance', 'product_uid'], axis=1).values
+    X_test_len = len(X_test)
+
+    if train_model:
+        rf = RandomForestRegressor(
+                n_estimators=30, max_depth=6, random_state=0)
+        clf = BaggingRegressor(
+                rf, n_estimators=60, max_samples=0.13,
+                random_state=25)
+        clf.fit(X_train, y_train)
+        y_prediction = clf.predict(X_test)
+
+        # Output the result
+        pd.DataFrame({"id": id_test, "relevance": y_prediction}) \
+            .to_csv('submission.csv', index=False)
+
+    # output the result to libSVM files.
+    validation_num = train_num / 4
+    dump_svmlight_file(X_train[: train_num - validation_num],
+                       y_train[: train_num - validation_num],
+                       'train_libSVM.dat', zero_based=True,
+                       multilabel=False)
+    dump_svmlight_file(X_train[train_num - validation_num:],
+                       y_train[train_num - validation_num:],
+                       'validate_libSVM.dat', zero_based=True,
+                       multilabel=False)
+    test_file_label = np.zeros(X_test_len)
+    dump_svmlight_file(X_test, test_file_label, 'test_libSVM.dat',
+                       zero_based=True, multilabel=False)
+
+
+if __name__ == '__main__':
+    main()
