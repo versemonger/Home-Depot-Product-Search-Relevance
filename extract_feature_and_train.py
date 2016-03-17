39f29118039b4bdd3b3cb15ba9c0ad9f97c2c1f3
versemonger <halinzhuo@gmail.com>
2016-03-16 23:45:25 -0600

    Make prediction with optimized parameters

--- extract_feature_and_train.py
+++ extract_feature_and_train.py
@@ -132,9 +132,10 @@ def main():
         # Output the result
         pd.DataFrame({"id": id_test, "relevance": y_prediction}) \
             .to_csv('submission.csv', index=False)
-    #
+    # Builder scorer which is used to do grid search for XGBoost
     RMSE = make_scorer(fmean_squared_error,
                        greater_is_better=False)
+    # Set XGBRegressor with optimized parameters.
     xgb_model\
         = xgb.XGBRegressor(learning_rate=0.06, silent=True,
                            objective="reg:logistic", gamma=2.25,
@@ -146,21 +147,30 @@ def main():
     #              'gamma': [2.20, 2.25, 2.3, 2.35],
     #          'min_child_weight': [0.3, 0.6, 1, 2]
     #          'colsample_bytree': [0.4, 0.45, 0.5, 0.55]
-    param_grid = {
-                  'learning_rate': [0.005, 0.01, 0.03, 0.06],
-                  'min_child_weight': [0.5, 1, 1.5, 2]
-                  }
+    # param_grid = {
+    #               'learning_rate': [0.005, 0.01, 0.03, 0.06],
+    #               'min_child_weight': [0.5, 1, 1.5, 2]
+    #               }
+    # # Do grid search with a set of parameters for XGBoost.
+    # model \
+    #     = grid_search\
+    #     .GridSearchCV(estimator=xgb_model, param_grid=param_grid,
+    #                   n_jobs=-1, cv=2, verbose=20, scoring=RMSE)
+    # print 'start search'
+    # model.fit(X_train, y_train)
+    # print("Best parameters found by grid search:")
+    # print(model.best_params_)
+    # print("Best CV score:")
+    # print(-model.best_score_)
 
-    model \
-        = grid_search\
-        .GridSearchCV(estimator=xgb_model, param_grid=param_grid,
-                      n_jobs=-1, cv=2, verbose=20, scoring=RMSE)
-    print 'start search'
-    model.fit(X_train, y_train)
-    print("Best parameters found by grid search:")
-    print(model.best_params_)
-    print("Best CV score:")
-    print(-model.best_score_)
+    # make predictions with tuned parameters.
+    xgb_model.fit(X_train, y_train)
+    prediction = xgb_model.predict(X_test)
+    prediction = prediction * 2 + 1
+    prediction[prediction > 3] = 3
+    prediction[prediction < 1] = 1
+    pd.DataFrame({"id": id_test, "relevance": prediction}) \
+        .to_csv('submission.csv', index=False)
     # output the result to libSVM files.
     validation_num = train_num / 4
     dump_svmlight_file(X_train[: train_num - validation_num],
