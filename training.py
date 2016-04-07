84b46293d3567225a5cfdc1c75ac18ed506c5137
versemonger <halinzhuo@gmail.com>
2016-04-06 21:57:22 -0600

    Disable random forest regressor for the moment.

--- training.py
+++ training.py
@@ -1,6 +1,5 @@
 from sklearn.ensemble import RandomForestRegressor
 import xgboost as xgb
-from sklearn.neural_network import MLPRegressor
 import numpy as np
 import pandas as pd
 import sys
@@ -8,11 +7,14 @@ import sys
 
 def main():
     X_train = np.load('X_train_with_SVD.npy')
-    X_test = pd.load('X_test_with_SVD.npy')
-    y_train = pd.load('Y_train.npy')
+    X_test = np.load('X_test_with_SVD.npy')
+    y_train = np.load('Y_train.npy')
     rf = RandomForestRegressor(n_estimators=60, max_depth=8,
                                random_state=7, n_jobs=-1)
-    rf.fit(X_train, y_train)
+    #################Temporarily disable RF#######
+    # rf.fit(X_train, y_train)
+    # Fit the data with RF
+    print "Fit the data with Random Forest Regressor"
     rfr_prediction = rf.predict(X_test)
 
     # # Output the result
@@ -21,14 +23,15 @@ def main():
 
     # Set XGBRegressor with optimized parameters.
     xgb_model\
-        = xgb.XGBRegressor(learning_rate=0.025, silent=True,
+        = xgb.XGBRegressor(learning_rate=0.03, silent=True,
                            objective="reg:logistic", gamma=2.15,
                            min_child_weight=5, subsample=0.8,
-                           colsample_bytree=0.8, n_estimators=101,
+                           colsample_bytree=0.8, n_estimators=900,
                            max_depth=9)
 
     # make predictions with tuned parameters and XGBoost model
     xgb_model.fit(X_train, y_train)
+    print 'Fit the data with XGBoost'
     xgb_prediction = xgb_model.predict(X_test)
 
     # # Use MPLRegressor
@@ -42,7 +45,9 @@ def main():
     print xgb_prediction[:20]
     print rfr_prediction[:20]
     # ensemble result of two models
-    prediction = (xgb_prediction + rfr_prediction) / 2
+    ################ Temporarily use only XGBoost #######
+    # prediction = (xgb_prediction + rfr_prediction) / 2
+    prediction = xgb_prediction
 
     # rescale the result to [1,3]
     prediction = prediction * 2 + 1
